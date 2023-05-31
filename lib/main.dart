ec4366879f801790dc8ca8a056942c697d3bb314
Akyl <kurmanalievakyl5@gmail.com>
2023-05-31 14:00:49 +0600

    first flutter practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,203 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyWidget());
-}
-//1
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+// void main() {
+//   // runApp(const MyWidget());
+// }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          color: Colors.green,
-        )
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text("My first app"),  
-          centerTitle: true,
-        ),
-        body: Container(
-          width: 300,
-          height: 300,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: const Color.fromARGB(255, 92, 161, 219),
-          ),
-          margin: const EdgeInsets.only(top: 150, left: 40),
-          child: const Text(
-            "My name is Akyl",
-            style: TextStyle(
-              color: Colors.purple,
-              fontSize: 35,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+  
+ 
+  //1
+  // // This widget is the root of your application.
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     theme: ThemeData(
+  //       appBarTheme: const AppBarTheme(
+  //         color: Colors.green,
+  //       )
+  //     ),
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         backgroundColor: Colors.green,
+  //         title: const Text("My first app"),  
+  //         centerTitle: true,
+  //       ),
+  //       body: Container(
+  //         width: 300,
+  //         height: 300,
+  //         alignment: Alignment.center,
+  //         decoration: BoxDecoration(
+  //           borderRadius: BorderRadius.circular(50),
+  //           color: const Color.fromARGB(255, 92, 161, 219),
+  //         ),
+  //         margin: const EdgeInsets.only(top: 150, left: 40),
+  //         child: const Text(
+  //           "My name is Akyl",
+  //           style: TextStyle(
+  //             color: Colors.purple,
+  //             fontSize: 35,
+  //             fontWeight: FontWeight.bold,
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+
+
+  //2
+  // @override
+  // Widget build(BuildContext context) {
+  //    return MaterialApp(
+  //     theme: ThemeData(
+  //       appBarTheme: const AppBarTheme(
+  //         color: Colors.green,
+  //       )
+  //     ),
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         backgroundColor: Colors.blue,
+  //         title: const Text("My first app"),  
+  //         centerTitle: true,
+  //       ),
+  //       body: Container(
+  //         width: 300,
+  //         height: 300,
+  //         alignment: Alignment.center,
+  //         decoration: BoxDecoration(
+  //           color: const Color.fromARGB(255, 137, 192, 238),
+  //         ),
+  //         margin: EdgeInsets.only(top: 150, left: 50),
+  //         padding: EdgeInsets.only(top: 270, left: 140),
+  //         child: Text(
+  //           "This is container",
+  //           style: TextStyle(
+  //             color: Colors.purple,
+  //             fontSize: 20,
+  //           ),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+
+
+  //3
+//  @override
+//   Widget build(BuildContext context) {
+//      return MaterialApp(
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(
+//           color: Colors.green,
+//         )
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: const Text("My first app"),  
+//           centerTitle: true,
+//         ),
+//         body: Container(
+//           width: 300,
+//           height: 300,
+//           alignment: Alignment.center,
+//           decoration: BoxDecoration(
+//             color: const Color.fromARGB(255, 137, 192, 238),
+//           ),
+//           margin: EdgeInsets.only(top: 150, left: 50),
+//           child: Text(
+//             "This is container",
+//             style: TextStyle(
+//               color: Colors.purple,
+//               fontSize: 20,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+
+//4
+// @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(
+//           color: Colors.blue,
+//         )
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: const Text("My first app"),
+//           centerTitle: true,
+//         ),
+//         body: Container(
+//           width: 300,
+//           height: 300,
+//           alignment: Alignment.center,
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(150),
+//             color: Colors.red,
+//             border: Border.all(
+//               color: Color.fromARGB(255, 150, 196, 233),
+//               width: 40,
+//             )
+//           ),
+//           margin: const EdgeInsets.only(top: 150, left: 40),
+//           child: const Text(
+//             "ITC BOOTCAMP",
+//             style: TextStyle(
+//               fontSize: 20,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+
+
+//5
+// @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(
+//           color: Colors.blue,
+//         )
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: const Text("My first app"),
+//           centerTitle: true,
+//         ),
+//         body: Container(
+//           width: 300,
+//           height: 300,
+//           alignment: Alignment.center,
+//           decoration: BoxDecoration(
+//             color: Colors.red,
+//             border: Border.all(
+//               color: Color.fromARGB(255, 150, 196, 233),
+//               width: 30,
+//             )
+//           ),
+//           margin: const EdgeInsets.only(top: 150, left: 40),
+//           child: const Text(
+//             "ITC BOOTCAMP",
+//             style: TextStyle(
+//               fontSize: 20,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
